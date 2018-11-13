67a39ffff255b1a062131f03d380d0c8454395d8
Xinyu Wen <xy.wen@icloud.com>
2018-11-12 19:52:26 -0800

    Update 362_Project.cpp

--- 362_Project.cpp
+++ 362_Project.cpp
@@ -38,6 +38,8 @@ bool mapToFile(char filename[], map<string, string> &fileMap);
 bool fileToMap(char filename[], map<string, string> &fileMap);
 void splitString(vector<string> &v_str, string str, char ch);
 void addLabel(char filename[]);
+vector<string> find_addresses(string file_name);
+void check_out(char* src, string manifest, char* dest);
 int num_of_manifest;
 
 int main(int argc, char *argv[]) {
